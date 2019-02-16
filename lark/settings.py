7303e24121c8d36e63de1567dfcc7b0e3fca6d7e
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-02-16 16:05:04 +0200

    minor changes

--- lark/settings.py
+++ lark/settings.py
@@ -144,8 +144,8 @@ AWS_LOCATION = 'static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'lark/static'),
 ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # for media storage
 
